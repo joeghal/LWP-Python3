47a409493986498b77ad742837e0bc757d532c1d
joeghal <80979879+joeghal@users.noreply.github.com>
2021-06-25 11:39:16 +0300

    Add files via upload

---
+++ Chapter 3 - Math,String,Object.py
@@ -0,0 +1,42 @@
+from math import *
+
+x = 10.9
+print(ceil(x))
+print(floor(x))
+x = 5
+print(type(pow(x, 2))) #x to the power of 2
+print(type(x ** 2))
+
+print(factorial(x)) # 5 * 4 * 3 * 2 * 1
+
+print(log2(1024))
+print(log10(1000))
+
+print(sqrt(9))
+
+print(sin(radians(90)))
+print(cos(radians(45)))
+
+print(pi)
+r = 2.5
+area = pi * pow(r,2)
+print(area)
+
+x = 'Hello World'
+print(x)
+y = """This is a very long 
+string that extends to many
+lines and can even go 
+further
+"""
+print(y)
+print(x[4])
+str1 = x[1:7]
+print(str1)
+
+print(len(str1))
+print(x.upper())
+print(x.lower())
+
+
+
