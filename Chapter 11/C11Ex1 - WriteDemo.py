a1df1754ab458ff9db8e0f0e9d8a811b422e5b42
joeghal <80979879+joeghal@users.noreply.github.com>
2021-06-25 13:06:20 +0300

    Add files via upload

---
+++ Chapter 11/C11Ex1 - WriteDemo.py
@@ -0,0 +1,14 @@
+def main():
+    # Open file for output
+    outfile = open("Presidents.txt", "w")
+
+    # Write data to the file
+    outfile.write("Bill Clinton\n")
+    outfile.write("George Bush\n")
+    outfile.write("Barack Obama\n")
+    outfile.write("Donald Trump")
+
+    outfile.close() # Close the output file
+
+main() # Call the main function
+
---
+++ Chapter 11/C11Ex9 - TestCircleWithException.py
@@ -0,0 +1,13 @@
+from CircleWithException import Circle
+
+try:
+    c1 = Circle(5)
+    print("c1's area is", c1.getArea())
+
+    c3 = Circle(0)
+    print("c3's area is", c3.getArea())
+    c2 = Circle(-5)
+    print("c2's area is", c2.getArea())
+
+except RuntimeError:
+    print("Invalid radius")
