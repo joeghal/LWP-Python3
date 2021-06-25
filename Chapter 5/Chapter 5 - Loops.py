ed1ec9357ed107b6f163856377f9db076aea4cec
joeghal <80979879+joeghal@users.noreply.github.com>
2021-06-25 12:58:47 +0300

    Add files via upload

---
+++ Chapter 5/Chapter 5 - Loops.py
@@ -0,0 +1,36 @@
+# for i in range(5):
+#     print(i)
+# for j in range(2, 8):
+#     print(j)
+
+# for x in range(2,10, 2):
+#     print(x)
+#
+
+# count = 0
+#
+# while count < 5:
+#     print(count)
+#     count = count +1
+
+
+# for i in range(10):
+#     if i % 2 != 0:
+#         #this is an odd number
+#         continue
+#     print(i)
+# for i in range(10):
+#     if i % 2 == 0:
+#         print(i)
+
+count = 0
+while count <5:
+    print(count)
+    if count % 2 != 0:
+        break
+    count = count + 1
+
+
+
+
+
