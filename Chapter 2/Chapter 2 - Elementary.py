ec978e1cbdfe6899ed16fb6a977b173e36bcf899
joeghal <80979879+joeghal@users.noreply.github.com>
2021-06-25 12:57:38 +0300

    Add files via upload

---
+++ Chapter 2/Chapter 2 - Elementary.py
@@ -0,0 +1,27 @@
+x = 5
+print(type(x))
+
+y = "Hello World"
+print(y)
+print(type(y))
+
+z = 2.5
+print(type(z))
+
+t = True
+print(type(t))
+
+print(x + z)
+print(x * z)
+
+f = 15
+o = 2
+print(f / o)
+print(f // o)
+print(f % o) #Remainder of division (f / o) = (15 /2) = 7.5 ==> (0.5 *2) =1
+
+print(f + o * x) # 15 + 2 * 5 = 25
+print((f + o) * x ) # (15 +2) * 5 = 85
+
+
+
